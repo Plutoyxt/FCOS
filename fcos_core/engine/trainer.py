06e8f89fccadad3435e4701104c9e41f84c264e3
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-08-25 11:56:23 +0800

    Update trainer.py

--- fcos_core/engine/trainer.py
+++ fcos_core/engine/trainer.py
@@ -2,13 +2,57 @@
 import datetime
 import logging
 import time
-
 import torch
 import torch.distributed as dist
-
+import os
+import argparse
+import torch
+from fcos_core.config import cfg
+from fcos_core.data import make_data_loader
+from fcos_core.solver import make_lr_scheduler
+from fcos_core.solver import make_optimizer
+from fcos_core.engine.inference import inference
+from fcos_core.modeling.detector import build_detection_model
+from fcos_core.utils.checkpoint import DetectronCheckpointer
+from fcos_core.utils.collect_env import collect_env_info
+from fcos_core.utils.comm import synchronize, \
+    get_rank, is_pytorch_1_1_0_or_later
+from fcos_core.utils.imports import import_file
+from fcos_core.utils.logger import setup_logger
+from fcos_core.utils.miscellaneous import mkdir
 from fcos_core.utils.comm import get_world_size, is_pytorch_1_1_0_or_later
 from fcos_core.utils.metric_logger import MetricLogger
 
+def run_test(cfg, model, distributed):
+    if distributed:
+        model = model.module
+    torch.cuda.empty_cache()  # TODO check if it helps
+    iou_types = ("bbox",)
+    if cfg.MODEL.MASK_ON:
+        iou_types = iou_types + ("segm",)
+    if cfg.MODEL.KEYPOINT_ON:
+        iou_types = iou_types + ("keypoints",)
+    output_folders = [None] * len(cfg.DATASETS.TEST)
+    dataset_names = cfg.DATASETS.TEST
+    if cfg.OUTPUT_DIR:
+        for idx, dataset_name in enumerate(dataset_names):
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name)
+            mkdir(output_folder)
+            output_folders[idx] = output_folder
+    data_loaders_val = make_data_loader(cfg, is_train=False, is_distributed=distributed)
+    for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
+        inference(
+            model,
+            data_loader_val,
+            dataset_name=dataset_name,
+            iou_types=iou_types,
+            box_only=False if cfg.MODEL.FCOS_ON or cfg.MODEL.RETINANET_ON else cfg.MODEL.RPN_ONLY,
+            device=cfg.MODEL.DEVICE,
+            expected_results=cfg.TEST.EXPECTED_RESULTS,
+            expected_results_sigma_tol=cfg.TEST.EXPECTED_RESULTS_SIGMA_TOL,
+            output_folder=output_folder,
+        )
+        synchronize()
 
 def reduce_loss_dict(loss_dict):
     """
@@ -53,6 +97,47 @@ def do_train(
     model.train()
     start_training_time = time.time()
     end = time.time()
+
+    parser = argparse.ArgumentParser(description="PyTorch Object Detection Training")
+    parser.add_argument(
+        "--config-file",
+        default="",
+        metavar="FILE",
+        help="path to config file",
+        type=str,
+    )
+    parser.add_argument("--local_rank", type=int, default=0)
+    parser.add_argument(
+        "--skip-test",
+        dest="skip_test",
+        help="Do not test the final model",
+        action="store_true",
+    )
+    parser.add_argument(
+        "opts",
+        help="Modify config options using the command-line",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+
+    args = parser.parse_args()
+
+    num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
+    args.distributed = num_gpus > 1
+
+    if args.distributed:
+        torch.cuda.set_device(args.local_rank)
+        torch.distributed.init_process_group(
+            backend="nccl", init_method="env://"
+        )
+        synchronize()
+
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+
+
+
     pytorch_1_1_0_or_later = is_pytorch_1_1_0_or_later()
     for iteration, (images, targets, _) in enumerate(data_loader, start_iter):
         data_time = time.time() - end
@@ -109,6 +194,8 @@ def do_train(
             )
         if iteration % checkpoint_period == 0:
             checkpointer.save("model_{:07d}".format(iteration), **arguments)
+            run_test(cfg, model, args.distributed)
+            model.train()
         if iteration == max_iter:
             checkpointer.save("model_final", **arguments)
 
@@ -119,3 +206,4 @@ def do_train(
             total_time_str, total_training_time / (max_iter)
         )
     )
+
