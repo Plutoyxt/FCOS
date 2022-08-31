9b3f1edf61d3895759b2addc08d34d39b2912433
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-08-31 19:33:08 +0800

    Update train_net.py

--- tools/train_net.py
+++ tools/train_net.py
@@ -28,63 +28,29 @@ from fcos_core.utils.comm import synchronize, \
 from fcos_core.utils.imports import import_file
 from fcos_core.utils.logger import setup_logger
 from fcos_core.utils.miscellaneous import mkdir
-
-
-def train(cfg, local_rank, distributed):
-    model = build_detection_model(cfg)
-    device = torch.device(cfg.MODEL.DEVICE)
-    model.to(device)
-
-    if cfg.MODEL.USE_SYNCBN:
-        assert is_pytorch_1_1_0_or_later(), \
-            "SyncBatchNorm is only available in pytorch >= 1.1.0"
-        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
-
-    optimizer = make_optimizer(cfg, model)
-    scheduler = make_lr_scheduler(cfg, optimizer)
-
-    if distributed:
-        model = torch.nn.parallel.DistributedDataParallel(
-            model, device_ids=[local_rank], output_device=local_rank,
-            # this should be removed if we update BatchNorm stats
-            broadcast_buffers=False,
-        )
-
-    arguments = {}
-    arguments["iteration"] = 0
-
-    output_dir = cfg.OUTPUT_DIR
-
-    save_to_disk = get_rank() == 0
-    checkpointer = DetectronCheckpointer(
-        cfg, model, optimizer, scheduler, output_dir, save_to_disk
-    )
-    extra_checkpoint_data = checkpointer.load(cfg.MODEL.WEIGHT)
-    arguments.update(extra_checkpoint_data)
-
-    data_loader = make_data_loader(
-        cfg,
-        is_train=True,
-        is_distributed=distributed,
-        start_iter=arguments["iteration"],
-    )
-
-
-    checkpoint_period = cfg.SOLVER.CHECKPOINT_PERIOD
-
-    do_train(
-        model,
-        data_loader,
-        optimizer,
-        scheduler,
-        checkpointer,
-        device,
-        checkpoint_period,
-        arguments,
-    )
-
-    return model
-
+import datetime
+import logging
+import time
+import torch
+import torch.distributed as dist
+import os
+import argparse
+import torch
+from fcos_core.config import cfg
+from fcos_core.data import make_data_loader
+from fcos_core.solver import make_lr_scheduler
+from fcos_core.solver import make_optimizer
+from fcos_core.engine.inference import inference
+from fcos_core.modeling.detector import build_detection_model
+from fcos_core.utils.checkpoint import DetectronCheckpointer
+from fcos_core.utils.collect_env import collect_env_info
+from fcos_core.utils.comm import synchronize, \
+    get_rank, is_pytorch_1_1_0_or_later
+from fcos_core.utils.imports import import_file
+from fcos_core.utils.logger import setup_logger
+from fcos_core.utils.miscellaneous import mkdir
+from fcos_core.utils.comm import get_world_size, is_pytorch_1_1_0_or_later
+from fcos_core.utils.metric_logger import MetricLogger
 
 def run_test(cfg, model, distributed):
     if distributed:
@@ -117,8 +83,50 @@ def run_test(cfg, model, distributed):
         )
         synchronize()
 
+def reduce_loss_dict(loss_dict):
+    """
+    Reduce the loss dictionary from all processes so that process with rank
+    0 has the averaged results. Returns a dict with the same fields as
+    loss_dict, after reduction.
+    """
+    world_size = get_world_size()
+    if world_size < 2:
+        return loss_dict
+    with torch.no_grad():
+        loss_names = []
+        all_losses = []
+        for k in sorted(loss_dict.keys()):
+            loss_names.append(k)
+            all_losses.append(loss_dict[k])
+        all_losses = torch.stack(all_losses, dim=0)
+        dist.reduce(all_losses, dst=0)
+        if dist.get_rank() == 0:
+            # only main process gets accumulated, so only divide by
+            # world_size in this case
+            all_losses /= world_size
+        reduced_losses = {k: v for k, v in zip(loss_names, all_losses)}
+    return reduced_losses
 
-def main():
+
+def do_train(
+    model,
+    data_loader,
+    optimizer,
+    scheduler,
+    checkpointer,
+    device,
+    checkpoint_period,
+    arguments,
+):
+    logger = logging.getLogger("fcos_core.trainer")
+    logger.info("Start training")
+    meters = MetricLogger(delimiter="  ")
+    max_iter = len(data_loader)
+    start_iter = arguments["iteration"]
+    model.train()
+    start_training_time = time.time()
+    end = time.time()
+
     parser = argparse.ArgumentParser(description="PyTorch Object Detection Training")
     parser.add_argument(
         "--config-file",
@@ -157,27 +165,74 @@ def main():
     cfg.merge_from_list(args.opts)
     cfg.freeze()
 
-    output_dir = cfg.OUTPUT_DIR
-    if output_dir:
-        mkdir(output_dir)
-
-    logger = setup_logger("fcos_core", output_dir, get_rank())
-    logger.info("Using {} GPUs".format(num_gpus))
-    logger.info(args)
-
-    logger.info("Collecting env info (might take some time)")
-    logger.info("\n" + collect_env_info())
-
-    logger.info("Loaded configuration file {}".format(args.config_file))
-    with open(args.config_file, "r") as cf:
-        config_str = "\n" + cf.read()
-        logger.info(config_str)
-    logger.info("Running with config:\n{}".format(cfg))
-    model = train(cfg, args.local_rank, args.distributed)
-
-    if not args.skip_test:
-        run_test(cfg, model, args.distributed)
 
 
-if __name__ == "__main__":
-    main()
+    pytorch_1_1_0_or_later = is_pytorch_1_1_0_or_later()
+    for iteration, (images, targets, _) in enumerate(data_loader, start_iter):
+        data_time = time.time() - end
+        iteration = iteration + 1
+        arguments["iteration"] = iteration
+
+        # in pytorch >= 1.1.0, scheduler.step() should be run after optimizer.step()
+        if not pytorch_1_1_0_or_later:
+            scheduler.step()
+
+        images = images.to(device)
+        targets = [target.to(device) for target in targets]
+
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = reduce_loss_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+        meters.update(loss=losses_reduced, **loss_dict_reduced)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        if pytorch_1_1_0_or_later:
+            scheduler.step()
+
+        batch_time = time.time() - end
+        end = time.time()
+        meters.update(time=batch_time, data=data_time)
+
+        eta_seconds = meters.time.global_avg * (max_iter - iteration)
+        eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+
+        if iteration % 20 == 0 or iteration == max_iter:
+            logger.info(
+                meters.delimiter.join(
+                    [
+                        "eta: {eta}",
+                        "iter: {iter}",
+                        "{meters}",
+                        "lr: {lr:.6f}",
+                        "max mem: {memory:.0f}",
+                    ]
+                ).format(
+                    eta=eta_string,
+                    iter=iteration,
+                    meters=str(meters),
+                    lr=optimizer.param_groups[0]["lr"],
+                    memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
+                )
+            )
+        if iteration % checkpoint_period == 0:
+            checkpointer.save("model_{:07d}".format(iteration), **arguments)
+            run_test(cfg, model, args.distributed)
+            model.train()
+        if iteration == max_iter:
+            checkpointer.save("model_final", **arguments)
+
+    total_training_time = time.time() - start_training_time
+    total_time_str = str(datetime.timedelta(seconds=total_training_time))
+    logger.info(
+        "Total training time: {} ({:.4f} s / it)".format(
+            total_time_str, total_training_time / (max_iter)
+        )
+    )
+
