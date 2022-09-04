7b22ecbe903ac810dd08bec7eaf6b5598c44d9f8
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-09-04 21:43:44 +0800

    Update paths_catalog.py

--- fcos_core/config/paths_catalog.py
+++ fcos_core/config/paths_catalog.py
@@ -7,24 +7,24 @@ class DatasetCatalog(object):
     DATA_DIR = "/content"
     DATASETS = {
         "coco_2017_train": {
-            #"img_dir": "gdrive/MyDrive/train2 coco",
-            "img_dir": "ssdd-coco 2/images/train",
+            "img_dir": "gdrive/MyDrive/train2 coco",
+            #"img_dir": "ssdd-coco 2/images/train",
             #"img_dir": "gdrive/MyDrive/HRSID_JPG/train_image", 
             #"img_dir": "gdrive/MyDrive/ssdd_coco/train/train_image",#after_
             "ann_file": "ssdd-coco 2/annotations/train2017/trainval.json"
             #"ann_file": "gdrive/MyDrive/HRSID_JPG/annotations/train2017.json"
         },
         "coco_2017_val": {
-            "img_dir": "ssdd-coco 2/images/test",
-            # "img_dir": "gdrive/MyDrive/val2 coco", 
+            #"img_dir": "ssdd-coco 2/images/test",
+             "img_dir": "gdrive/MyDrive/val2 coco", 
             #"img_dir": "gdrive/MyDrive/ssdd_coco/test/test_image",
             "ann_file": "ssdd-coco 2/annotations/test2017/test.json"
             #"img_dir": "gdrive/MyDrive/HRSID_JPG/test_image",
             #"ann_file": "gdrive/MyDrive/HRSID_JPG/annotations/test2017.json"
         },
         "coco_2017_test_dev": {
-            "img_dir": "ssdd-coco 2/images/test",
-            #"img_dir": "gdrive/MyDrive/val2 coco", 
+            #"img_dir": "ssdd-coco 2/images/test",
+            "img_dir": "gdrive/MyDrive/val2 coco", 
             #"img_dir": "gdrive/MyDrive/ssdd_coco/test/test_image",
             "ann_file": "ssdd-coco 2/annotations/test2017/test.json"
             #"img_dir": "gdrive/MyDrive/HRSID_JPG/test_image",
