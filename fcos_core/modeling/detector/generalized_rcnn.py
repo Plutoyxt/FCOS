e5e4faca7b508c73bc187e75065397a9965a07a6
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-10-07 16:28:41 +0800

    Update generalized_rcnn.py

--- fcos_core/modeling/detector/generalized_rcnn.py
+++ fcos_core/modeling/detector/generalized_rcnn.py
@@ -23,30 +23,28 @@ class GeneralizedRCNN(nn.Module):
         detections / masks from it.
     """
 
-    def __init__(self, cfg, targets):
+    def __init__(self, cfg):
         super(GeneralizedRCNN, self).__init__()
 
-        self.backbone = build_backbone(cfg,targets)
+        self.backbone = build_backbone(cfg)
         self.rpn = build_rpn(cfg, self.backbone.out_channels)
         self.roi_heads = build_roi_heads(cfg, self.backbone.out_channels)
 
-    def forward(self, images):
+    def forward(self, images, targets=None):
         """
         Arguments:
             images (list[Tensor] or ImageList): images to be processed
             targets (list[BoxList]): ground-truth boxes present in the image (optional)
-
         Returns:
             result (list[BoxList] or dict[Tensor]): the output from the model.
                 During training, it returns a dict[Tensor] which contains the losses.
                 During testing, it returns list[BoxList] contains additional fields
                 like `scores`, `labels` and `mask` (for Mask R-CNN models).
-
         """
         if self.training and targets is None:
             raise ValueError("In training mode, targets should be passed")
         images = to_image_list(images)
-        features = self.backbone(images.tensors,targets)
+        features = self.backbone(images.tensors)
         proposals, proposal_losses = self.rpn(images, features, targets)
         if self.roi_heads:
             x, result, detector_losses = self.roi_heads(features, proposals, targets)
