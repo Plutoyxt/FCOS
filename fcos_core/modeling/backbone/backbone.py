d86c295b3b6ae8143c61e5321b189135fd1203ad
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-10-07 17:10:12 +0800

    Update backbone.py

--- fcos_core/modeling/backbone/backbone.py
+++ fcos_core/modeling/backbone/backbone.py
@@ -24,7 +24,7 @@ def build_resnet_backbone(cfg):
 @registry.BACKBONES.register("R-50-FPN")
 @registry.BACKBONES.register("R-101-FPN")
 @registry.BACKBONES.register("R-152-FPN")
-def build_resnet_fpn_backbone(cfg,targets=None):
+def build_resnet_fpn_backbone(cfg):
     body = resnet.ResNet(cfg)
     in_channels_stage2 = cfg.MODEL.RESNETS.RES2_OUT_CHANNELS
     out_channels = cfg.MODEL.RESNETS.BACKBONE_OUT_CHANNELS
@@ -40,7 +40,6 @@ def build_resnet_fpn_backbone(cfg,targets=None):
             cfg.MODEL.FPN.USE_GN, cfg.MODEL.FPN.USE_RELU
         ),
         top_blocks=fpn_module.LastLevelMaxPool(),
-        targets=None,
     )
     model = nn.Sequential(OrderedDict([("body", body), ("fpn", fpn)]))
     model.out_channels = out_channels
@@ -49,7 +48,7 @@ def build_resnet_fpn_backbone(cfg,targets=None):
 
 @registry.BACKBONES.register("R-50-FPN-RETINANET")
 @registry.BACKBONES.register("R-101-FPN-RETINANET")
-def build_resnet_fpn_p3p7_backbone(cfg,targets=None):
+def build_resnet_fpn_p3p7_backbone(cfg):
     body = resnet.ResNet(cfg)
     in_channels_stage2 = cfg.MODEL.RESNETS.RES2_OUT_CHANNELS
     out_channels = cfg.MODEL.RESNETS.BACKBONE_OUT_CHANNELS
@@ -67,7 +66,6 @@ def build_resnet_fpn_p3p7_backbone(cfg,targets=None):
             cfg.MODEL.FPN.USE_GN, cfg.MODEL.FPN.USE_RELU
         ),
         top_blocks=fpn_module.LastLevelP6P7(in_channels_p6p7, out_channels),
-        targets=None,
     )
     model = nn.Sequential(OrderedDict([("body", body), ("fpn", fpn)]))
     model.out_channels = out_channels
@@ -75,7 +73,7 @@ def build_resnet_fpn_p3p7_backbone(cfg,targets=None):
 
 
 @registry.BACKBONES.register("MNV2-FPN-RETINANET")
-def build_mnv2_fpn_backbone(cfg,targets=None):
+def build_mnv2_fpn_backbone(cfg):
     body = mobilenet.MobileNetV2(cfg)
     in_channels_stage2 = body.return_features_num_channels
     out_channels = cfg.MODEL.RESNETS.BACKBONE_OUT_CHANNELS
@@ -91,16 +89,15 @@ def build_mnv2_fpn_backbone(cfg,targets=None):
             cfg.MODEL.FPN.USE_GN, cfg.MODEL.FPN.USE_RELU
         ),
         top_blocks=fpn_module.LastLevelP6P7(out_channels, out_channels),
-        targets=None,
     )
     model = nn.Sequential(OrderedDict([("body", body), ("fpn", fpn)]))
     model.out_channels = out_channels
     return model
 
 
-def build_backbone(cfg,targets=None):
+def build_backbone(cfg):
     assert cfg.MODEL.BACKBONE.CONV_BODY in registry.BACKBONES, \
         "cfg.MODEL.BACKBONE.CONV_BODY: {} are not registered in registry".format(
             cfg.MODEL.BACKBONE.CONV_BODY
         )
-    return registry.BACKBONES[cfg.MODEL.BACKBONE.CONV_BODY](cfg,targets=None)
+    return registry.BACKBONES[cfg.MODEL.BACKBONE.CONV_BODY](cfg)
